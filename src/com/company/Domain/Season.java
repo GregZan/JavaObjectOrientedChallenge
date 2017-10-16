62d0372ed535dbbcb1ffdeed87a6d0c021314dd6
Gregorio Zanin Sancho Goncalves (BFSI - Brazil) <GR389658@wipro.com>
2017-10-16 12:57:00 -0200

    Small fixes to fields missing and wrong format

--- src/com/company/Domain/Season.java
+++ src/com/company/Domain/Season.java
@@ -4,19 +4,14 @@ import java.util.ArrayList;
 
 public class Season extends tvShowDefinitions{
     private ArrayList<Episode> episodeList = new ArrayList<>();
-    private String name;
 
-    public Season(String summary, int airingDate, int rating, ArrayList<Episode> episodeList, String name) {
+    public Season(String summary, int airingDate, int rating, ArrayList<Episode> episodeList) {
         super(summary, airingDate, rating);
         this.episodeList = episodeList;
-        this.name = name;
     }
 
     public ArrayList<Episode> getEpisodeList() {
         return episodeList;
     }
 
-    public String getName() {
-        return name;
-    }
 }
--- src/com/company/Domain/Series.java
+++ src/com/company/Domain/Series.java
@@ -4,19 +4,25 @@ import java.util.ArrayList;
 
 public class Series extends tvShowDefinitions{
     private String name;
-    private ArrayList<Episode> seasonList = new ArrayList<>();
+    private ArrayList<Season> seasonList = new ArrayList<>();
+    private String genre;
 
-    public Series(String summary, int airingDate, int rating, String name, ArrayList<Episode> seasonList) {
+    public Series(String summary, int airingDate, int rating, String name, ArrayList<Season> seasonList, String genre) {
         super(summary, airingDate, rating);
         this.name = name;
         this.seasonList = seasonList;
+        this.genre = genre;
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Episode> getSeasonList() {
+    public ArrayList<Season> getSeasonList() {
         return seasonList;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 }
